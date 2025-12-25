63f4ae495ef2962a518df235563654f303be916c
Haya Almousa <hayaf2211@gmail.com>
2025-12-25 14:48:05 +0300

    m

---
+++ BakerApp/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  BakerApp
+//
+//  Created by Haya almousa on 25/12/2025.
+//
+
+import SwiftUI
+
+struct Course: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Course()
+}
