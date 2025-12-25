3f768ae5cb27ccf9fb3c92982a7aafd45773b97e
Haya Almousa <hayaf2211@gmail.com>
2025-12-25 14:43:07 +0300

    Initial Commit

---
+++ BakerApp/BakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakerAppApp.swift
+//  BakerApp
+//
+//  Created by Haya almousa on 25/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct BakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
