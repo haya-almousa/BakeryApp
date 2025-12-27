2b32bc8ab2ddb13fd9f5280293579f97abaf2811
Tala Aldhahri <toti_kd123@hotmail.com>
2025-12-27 20:35:36 +0300

    Added Course Levels color assests, added MVVM folders, and created course file

---
+++ BakerApp/ViewModels/Course.swift
@@ -0,0 +1,36 @@
+//
+//  Course.swift
+//  BakerApp
+//
+//  Created by Haya almousa on 25/12/2025.
+//
+
+import Foundation
+import SwiftUI
+
+//we made it string and codable so that it van be translated easily the text from json into swift objects
+enum CourseLevel: String, Codable{
+    case beginner = "Beginner"
+    case intermediate = "Intermediate"
+    case advanced = "Advanced"
+    
+    var themeColor: Color {
+        switch self {
+        case .beginner:
+            return Color("LevelBeginner")
+        case .intermediate:
+            return Color("LevelIntermediate")
+        case .advanced:
+            return Color("LevelAdvanced")
+        }
+    }
+}
+
+struct course: Identifiable, Codable {
+    let id: UUID
+    let title: String
+    let level: String
+    let duration: String
+    let date: String
+    let imageURL: String
+}
