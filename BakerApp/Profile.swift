96dd86d7683871aaba9a1dda4c8cd0a8305aeda4
Haya Almousa <hayaf2211@gmail.com>
2025-12-28 15:31:40 +0300

    k

---
+++ BakerApp/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  BakerApp
+//
+//  Created by Haya almousa on 28/12/2025.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
